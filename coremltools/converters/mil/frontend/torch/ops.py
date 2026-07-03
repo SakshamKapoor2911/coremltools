86f603f936e0d4b2e0d5ab5e283aaae02ff02732
SakshamKapoor2911 <sakshamkapoor2911@gmail.com>
2026-07-03 10:23:14 -0400

    Revert "Try yx deform conv resample coordinates"
    This reverts commit 4b644cfacc15d42a162636ff9ca57f555dacf491.

--- coremltools/converters/mil/frontend/torch/ops.py
+++ coremltools/converters/mil/frontend/torch/ops.py
@@ -9172,7 +9172,7 @@ def torchvision_deform_conv2d(context, node):
                 coord_x = mb.add(x=base_x, y=offset_x)
                 coord_y = mb.add(x=coord_y, y=1.0)
                 coord_x = mb.add(x=coord_x, y=1.0)
-                coordinates = mb.stack(values=[coord_y, coord_x], axis=-1)
+                coordinates = mb.stack(values=[coord_x, coord_y], axis=-1)
                 sampled = mb.resample(
                     x=x_group,
                     coordinates=coordinates,
