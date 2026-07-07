98181a6c821e62f34123f417636d6cceb9d98dff
SakshamKapoor2911 <sakshamkapoor2911@gmail.com>
2026-07-06 22:42:26 -0400

    Test deform conv with iOS16 target on macOS 14

--- coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
+++ coremltools/converters/mil/frontend/torch/test/test_torch_ops.py
@@ -13598,6 +13598,7 @@ class TestDeformConv2d(TorchBaseTest):
                 source="pytorch",
                 convert_to="milinternal",
                 inputs=converter_inputs,
+                minimum_deployment_target=ct.target.iOS16,
             )
             self._assert_deform_conv2d_ops(prog)
             return
@@ -13612,6 +13613,7 @@ class TestDeformConv2d(TorchBaseTest):
                 input_data,
                 backend=backend,
                 compute_unit=compute_unit,
+                minimum_deployment_target=ct.target.iOS16,
             )
             self._assert_deform_conv2d_ops(mlmodel._mil_program)
             return
@@ -13622,6 +13624,7 @@ class TestDeformConv2d(TorchBaseTest):
             input_as_shape=False,
             backend=backend,
             compute_unit=compute_unit,
+            minimum_deployment_target=ct.target.iOS16,
             atol=1e-4,
             rtol=1e-4,
             frontend=frontend,
